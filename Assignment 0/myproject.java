71764742ab76ab0ce8a0c9d702d5292bd481a2c5
kroax9797 <tejasmhaiskar@gmail.com>
2023-08-13 23:52:19 +0530

    Update myproject.java
    Minor changes and improvements

--- Assignment 0/myproject.java
+++ Assignment 0/myproject.java
@@ -1,12 +1,14 @@
 public class myproject{
     public static void main(String args[]){
-        int L = 5;
-        int w = 10;
-        double p = 0.90;
+        int L = 1000;
+        int w = 1;
+        double p = 0.99;
         int border[][] = new int[w][L];
 
         int dx[] = {1,1,1,0,0,-1,-1,-1};
-        int dy[] = {0,-1,1,-1,1,-1,1,0};;
+        int dy[] = {0,-1,1,-1,1,-1,1,0};
+
+        boolean visited[][] = new boolean[w][L];
 
         int start = (int)(Math.random()*L);
         int x = 0 ;
@@ -18,7 +20,7 @@ public class myproject{
         //sensor = 1 
         //infiltrator = 2   
 
-        while(border[x][y] != 1 || y == L-1){
+        while(x == w-1 || time < 10000){
             for(int i = 0 ; i < w ; i++){
                 for(int j = 0 ; j < L ; j++){
                     int on = -1 ; 
@@ -32,19 +34,22 @@ public class myproject{
                 }
             }
 
-        //infiltrator simulation 
+            //infiltrator simulation
             if(border[x][y] == 1){
-                //break;
-                //break out as it is caught 
+                break; 
             }
             else{
                 border[x][y] = 0 ; 
                 boolean flag = false ;
                 for(int k = 0 ; k < 8 ; k++){
                     if(x+dx[k] >= 0 && x+dx[k] < w && y+dy[k] >=0 && y+dy[k] < L){
-                        if(border[x+dx[k]][y+dy[k]] == 0 && flag == false){
-                            border[x+dx[k]][y+dy[k]] = 2 ;
+                        if(border[x+dx[k]][y+dy[k]] == 0 && flag == false && visited[x+dx[k]][y+dy[k]] == false){
+                            x = x + dx[k];
+                            y = y + dy[k];
+                            border[x][y] = 2 ;
                             flag = true ;
+                            visited[x][y] = true ;
+                            
                         }
                         if(flag == false && border[x+dx[k]][y+dy[k]]==1){
                             break ; 
@@ -58,6 +63,14 @@ public class myproject{
 
         //sensor simulation 
         System.out.println(time);
+        if(y != L){
+            System.out.println("Caught");
+        }
+        else{
+            System.out.println("Infiltrated!");
+        }
 
     }
 }
+
+
